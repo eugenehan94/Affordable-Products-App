cb71a0de2393f943640c6cb1b967e63966c20591
eugenehan94 <eugene.han94@gmail.com>
2023-04-19 18:47:39 -0400

    testing puppeteer

--- pages/api/getFlippItems.tsx
+++ pages/api/getFlippItems.tsx
@@ -1,25 +1,52 @@
-// Another web scrapper using cheerio
-//https://dev.to/mtliendo/create-a-public-api-by-web-scraping-in-nextjs-2f5n
-//https://www.youtube.com/watch?v=l9DPeGeWNYY&ab_channel=Raddy
-const cheerio = require("cheerio");
-
 // puppeteer?
 //https://www.youtube.com/watch?v=FKkDUW4E2Qc&ab_channel=Joshtriedcoding
 import { NextApiRequest, NextApiResponse } from "next/types";
 
-const getFlippItems = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const url = "https://flipp.com/search/chicken";
-    const response = await fetch(url);
-    const html = await response.text();
-    console.log("HTML: ", html)
-    const $ = await cheerio.load(html);
-    const wrapper = $(".item").text();
-    console.log("wrapper: ", wrapper);
-  } catch (error) {
-    console.log("ERROR: ", error);
-  }
+const puppeteer = require("puppeteer");
+import { Browser } from "puppeteer";
 
+const getFlippItems = async (req: NextApiRequest, res: NextApiResponse) => {
+  const url = "https://books.toscrape.com/";
+
+  const browser: Browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  const bookData = await page.evaluate((url) => {
+    const convertPrice = (price: string) => {
+      return parseFloat(price.replace("£", ""));
+    };
+
+    const convertRating = (rating: string) => {
+      switch (rating) {
+        case "One":
+          return 1;
+        case "Two":
+          return 2;
+        case "Three":
+          return 3;
+        case "Four":
+          return 4;
+        case "Five":
+          return 5;
+        default:
+          return 0;
+      }
+    };
+
+    const bookPods = Array.from(document.querySelectorAll(".product_pod"));
+    const data = bookPods.map((book: any) => ({
+      title: book.querySelector("h3 a").getAttribute("title"),
+      price: convertPrice(book.querySelector(".price_color").innerText),
+      imgSrc: url + book.querySelector("img").getAttribute("src"),
+      rating: convertRating(book.querySelector(".star-rating").classList[1]),
+    }));
+    return data;
+  }, url);
+
+  console.log("bookData: ", bookData);
+
+  await browser.close();
   // Receive the input from the req body
   // const body = JSON.parse(req.body);
   // const {input} = body
