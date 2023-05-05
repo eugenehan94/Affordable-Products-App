e9fc8f0c03d798f4693c3a3a8b632b7d1891070c
eugenehan94 <eugene.han94@gmail.com>
2023-05-05 19:07:00 -0400

    started Search page

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,4 +1,13 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 const Search = () => {
+  const router = useRouter();
+  const handleClick = () => {
+    console.log("Clicked");
+    router.push("/search/");
+  };
+
   return (
     <div className="px-4 my-16 flex justify-center items-center" id="search">
       <div className="w-10/12 md:w-3/5">
@@ -12,9 +21,17 @@ const Search = () => {
             placeholder="Try searching for 'eggs'"
             className="pl-2.5 pt-1.5 pr-1.5 pb-1.5 text-2xl border-gray-400 border-solid border-2 rounded"
           ></input>
-          <button className="cursor-pointer bg-blue-500 hover:bg-sky-400 text-white rounded p-1 mt-1">
+          {/* <Link
+            href="/search"
+            className="w-full h-10 mt-3 bg-blue-500 hover:bg-sky-400 rounded text-sm text-white flex justify-center items-center "
+          > */}
+          <button
+            onClick={() => handleClick()}
+            className="cursor-pointer bg-blue-500 hover:bg-sky-400 text-white rounded p-1 mt-1"
+          >
             Search Flipp
           </button>
+          {/* </Link> */}
         </div>
       </div>
     </div>
