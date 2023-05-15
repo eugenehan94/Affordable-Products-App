847299e63a50f5b6a96b44c2dbe6cef424c5a72c
eugenehan94 <eugene.han94@gmail.com>
2023-05-15 13:39:56 -0400

    added to sidebar component

--- components/SearchSidebar.tsx
+++ components/SearchSidebar.tsx
@@ -76,28 +76,103 @@ const SearchSidebar = ({ setSidebarOpen, sidebarOpen }: any) => {
             Coupons{" "}
           </div>
         </a>
-        <div className="pl-6 mt-5 overflow-y-auto flex flex-col">
-          <h2 className="uppercase text-xs">Flyer</h2>
-          <a>All Flyers</a>
-          <a>Groceries</a>
-          <a>Restaurants</a>
-          <a>Home & Garden</a>
-          <a>Pharmacy</a>
-          <a>General</a>
-          <a>Merchandise</a>
-          <a>Electronics</a>
-          <a>Baby & Kids</a>
-          <a>Fashion</a>
-          <a>Automotive</a>
-          <a>Sporting</a>
-          <a>Goods</a>
-          <a>Pets</a>
-          <a>Office</a>
-          <a>Specialty</a>
+        <div className="px-6 mt-5 overflow-y-auto flex flex-col h-80">
+          <h2 className="uppercase text-xs text-slate-500 font-semibold">
+            Flyers
+          </h2>
+          <a className=" flex justify-between items-center text-lg leading-8 cursor-pointer hover:bg-gray-100">
+            <span>All Flyers</span>
+            <span className="text-xs text-sky-800 bg-slate-200 p-px rounded-sm">
+              214
+            </span>
+          </a>
+          <a className=" flex justify-between items-center text-lg leading-8 cursor-pointer hover:bg-gray-100">
+            <span>Groceries</span>
+            <span className="text-xs text-sky-800 bg-slate-200 p-px rounded-sm">
+              90
+            </span>
+          </a>
+          <a className=" flex justify-between items-center text-lg leading-8 cursor-pointer hover:bg-gray-100">
+            <span>Restaurants</span>
+            <span className="text-xs text-sky-800 bg-slate-200 p-px rounded-sm">
+              1
+            </span>
+          </a>
+          <a className="flex justify-between items-center text-lg leading-8 cursor-pointer hover:bg-gray-100">
+            <span>Home & Garden</span>
+            <span className="text-xs text-sky-800 bg-slate-200 p-px rounded-sm">
+              50
+            </span>
+          </a>
+          <a className="flex justify-between items-center text-lg leading-8 cursor-pointer hover:bg-gray-100">
+            <span>Pharmacy</span>
+            <span className="text-xs text-sky-800 bg-slate-200 p-px rounded-sm">
+              17
+            </span>
+          </a>
+          <a className=" flex justify-between items-center text-lg leading-8 cursor-pointer hover:bg-gray-100">
+            <span>General Merchandise</span>
+            <span className="text-xs text-sky-800 bg-slate-200 p-px rounded-sm">
+              13
+            </span>
+          </a>
+          <a className="flex justify-between items-center text-lg leading-8 cursor-pointer hover:bg-gray-100">
+            <span>Electronics</span>
+            <span className="text-xs text-sky-800 bg-slate-200 p-px rounded-sm">
+              17
+            </span>
+          </a>
+          <a className="flex justify-between items-center text-lg leading-8 cursor-pointer hover:bg-gray-100">
+            <span>Baby & Kids</span>
+            <span className="text-xs text-sky-800 bg-slate-200 p-px rounded-sm">
+              5
+            </span>
+          </a>
+          <a className="flex justify-between items-center text-lg leading-8 cursor-pointer hover:bg-gray-100">
+            <span>Fashion</span>
+            <span className="text-xs text-sky-800 bg-slate-200 p-px rounded-sm">
+              9
+            </span>
+          </a>
+          <a className="flex justify-between items-center text-lg leading-8 cursor-pointer hover:bg-gray-100">
+            <span>Automotive</span>
+            <span className="text-xs text-sky-800 bg-slate-200 p-px rounded-sm">
+              7
+            </span>
+          </a>
+          <a className="flex justify-between items-center text-lg leading-8 cursor-pointer hover:bg-gray-100">
+            <span>Sporting Goods</span>
+            <span className="text-xs text-sky-800 bg-slate-200 p-px rounded-sm">
+              5
+            </span>
+          </a>
+          <a className="flex justify-between items-center text-lg leading-8 cursor-pointer hover:bg-gray-100">
+            <span>Pets</span>
+            <span className="text-xs text-sky-800 bg-slate-200 p-px rounded-sm">
+              7
+            </span>
+          </a>
+          <a className="flex justify-between items-center text-lg leading-8 cursor-pointer hover:bg-gray-100">
+            <span>Office</span>
+            <span className="text-xs text-sky-800 bg-slate-200 p-px rounded-sm">
+              3
+            </span>
+          </a>
+          <a className="flex justify-between items-center text-lg leading-8 cursor-pointer hover:bg-gray-100">
+            <span>Specialty</span>
+            <span className="text-xs text-sky-800 bg-slate-200 p-px rounded-sm">
+              3
+            </span>
+          </a>
+        </div>
+        <div className="flex justify-center">
+          <button className=" mt-5 px-4 bg-sky-600 text-white text-center w-24 h-10 font-bold text-sm">
+            Francais
+          </button>
         </div>
       </div>
       <div
-        className="w-2/4 bg-black opacity-50 hover:cursor-pointer"
+        className="w-2/4 bg-black opacity-50"
         onClick={() => {
           handleClick();
         }}
