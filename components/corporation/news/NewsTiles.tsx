8b15e49a53589f29fe1df32fae012d8740435b03
eugenehan94 <eugene.han94@gmail.com>
2023-07-15 12:44:42 -0400

    added component to corporation news page - NewsTiles.tsx

---
+++ components/corporation/news/NewsTiles.tsx
@@ -0,0 +1,47 @@
+const NewsTiles = () => {
+  return (
+    <section className="p-5 my-24 md:flex md:flex-wrap">
+      <article className="shadow-xl mb-16 md:basis-1/2">
+        <div className="mb-8 px-10">
+          <h3 className="text-sky-900 font-medium leading-relaxed md:text-lg">
+            PageSuite Partner with Flipp to Serve Digital Circulars within
+            e-Editions
+          </h3>
+        </div>
+        <div className="border-t border-gray-200 px-10 py-4">
+          <span className="text-2xl md:text-3xl lg:text-4xl text-gray-400 font-bold">
+            May 25, 2023
+          </span>
+        </div>
+      </article>
+      <article className="shadow-xl mb-16 md:basis-1/2">
+        <div className="mb-8 px-10">
+          <h3 className="text-sky-900 font-medium leading-relaxed md:text-lg">
+            AdAdapted and Flipp Partner To Help Brands Land Products on US
+            Shoppers’ Lists
+          </h3>
+        </div>
+        <div className="border-t border-gray-200 px-10 py-4">
+          <span className="text-2xl md:text-3xl lg:text-4xl text-gray-400 font-bold">
+            September 15, 2022
+          </span>
+        </div>
+      </article>
+      <article className="shadow-xl mb-16 md:basis-1/2">
+        <div className="mb-8 px-10">
+          <h3 className="text-sky-900 font-medium leading-relaxed md:text-lg">
+            Flipp Acquires reebee, Strengthening Position as Canada’s Largest
+            Distributor of Digital Savings Content
+          </h3>
+        </div>
+        <div className="border-t border-gray-200 px-10 py-4">
+          <span className="text-2xl md:text-3xl lg:text-4xl text-gray-400 font-bold">
+            June 22, 2022
+          </span>
+        </div>
+      </article>
+    </section>
+  );
+};
+
+export default NewsTiles;
