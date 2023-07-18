15e430c742349478b985417fb6423f044c6d0b58
eugenehan94 <eugene.han94@gmail.com>
2023-07-18 12:12:19 -0400

    added contact_us page

---
+++ pages/corporation/contact_us/index.tsx
@@ -0,0 +1,18 @@
+import Navbar from "@/components/corporation/_shared/Navbar";
+import Hero from "@/components/corporation/contact_us/Hero";
+import Form from "@/components/corporation/contact_us/Form";
+import Address from "@/components/corporation/contact_us/Address";
+import Footer from "@/components/corporation/_shared/Footer";
+const ContactUs = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Form />
+      <Address />
+      <Footer />
+    </>
+  );
+};
+
+export default ContactUs;
