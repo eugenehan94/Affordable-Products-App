db09db52abd51024a0022ec5fa304d3339fd2256
eugenehan94 <eugene.han94@gmail.com>
2023-07-20 13:16:22 -0400

    added component to culture page - CultureDescription.tsx

---
+++ components/corporation/culture/CultureDescription.tsx
@@ -0,0 +1,36 @@
+const CultureDescription = () => {
+  return (
+    <section className="mt-20 px-5 flex flex-col gap-y-10 md:flex-wrap md:flex-row md:gap-y-0">
+      <div className="md:basis-1/2 md:px-5">
+        <p className="font-medium leading-7 md:text-lg">
+          Culture is at the heart of everything we do. We would be nothing
+          without our people, and we do our best to remind them of that as often
+          as possible. We are driven by our mission to help shoppers provide for
+          their families by making life more affordable, while becoming the
+          number one source of savings and deals across North America. Not only
+          are we committed to our shoppers, but we are also committed to acting
+          as a trusted business advisor to our merchant partners to help them
+          win in the retail space.
+        </p>
+      </div>
+      <div className="md:basis-1/2 md:px-5 flex flex-col gap-y-5">
+        <p className="font-medium leading-7 md:text-lg">
+          Our team is a group of driven, motivated, and passionate individuals
+          who work collaboratively with one another to ensure we remain a
+          cohesive Flipp Team or “One Flipp Team,” as we often say. We believe
+          in pushing each other to the next level through continuous learning
+          and coaching to maintain a high growth mindset for ourselves, our
+          company, and our colleagues.
+        </p>
+        <p className="font-medium leading-7 md:text-lg">
+          You won’t find a team more tight-knit than ours, and that’s because we
+          are a team who live and breathe our cultural values and principles. We
+          are proud to say that we have been successful in maintaining our
+          one-of-a-kind culture, no matter how big our team grows!
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default CultureDescription;
