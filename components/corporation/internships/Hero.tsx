452c6af0329a7ee8753c1b29e1b3e3613412dddd
eugenehan94 <eugene.han94@gmail.com>
2023-07-28 16:52:50 -0400

    added component for internship page - Hero.tsx

---
+++ components/corporation/internships/Hero.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+const Hero = () => {
+  return (
+    <section className="mt-24 bg-yellow-400">
+      <div className="p-5 pt-12 md:flex md:p-8 md:pt-14">
+        <div className="md:basis-1/2 md:p-1">
+          <div className="mb-5">
+            <h2 className="uppercase text-violet-600 text-lg md:text-xl lg:text-2xl font-bold">
+              OUR TEAMS
+            </h2>
+          </div>
+          <div className="mb-5">
+            <h2 className="text-violet-700 text-2xl md:text-3xl lg:text-4xl font-bold lg:leading-snug">
+              Do you have what it takes to create the ultimate consumer
+              experience? Join our team!
+            </h2>
+          </div>
+          <div>
+            <button className="bg-white px-7 py-2.5 md:text-lg rounded-full text-violet-600 font-medium">
+              <Link href="/corporation/job_openings">View Job Openings</Link>
+            </button>
+          </div>
+        </div>
+        <div className="md:basis-1/2 relative">
+          <Image
+            src="/CorporationInternshipsHeroImage.jpg"
+            alt="Hero"
+            width={500}
+            height={500}
+            className="translate-y-10 md:translate-y-0"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
