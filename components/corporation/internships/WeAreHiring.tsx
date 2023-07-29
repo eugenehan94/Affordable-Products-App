699728dc9a5bd5032d5690d6975df57114e26e6e
eugenehan94 <eugene.han94@gmail.com>
2023-07-29 16:56:12 -0400

    added component for internship page - WeAreHiring.tsx

---
+++ components/corporation/internships/WeAreHiring.tsx
@@ -0,0 +1,18 @@
+const WeAreHiring = () => {
+  return (
+    <section className="p-5">
+      <div className="bg-teal-100/30 py-8">
+        <div className="mb-5">
+          <h2 className="text-xl md:text-2xl lg:text-3xl font-bold text-gray-600">We are hiring</h2>
+        </div>
+        <div>
+          <h2 className="text-xl text-gray-800 font-bold mb-5">Operations Intern</h2>
+          <p className="mb-5 md:text-lg">Read Full Description</p>
+          <p className="mb-5 md:text-lg">Apply Now</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WeAreHiring;
