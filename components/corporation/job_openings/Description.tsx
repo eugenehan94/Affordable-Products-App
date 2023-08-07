f752e29c15eaa93c2065ce9e0d68c3f9965f6a1d
eugenehan94 <eugene.han94@gmail.com>
2023-08-07 12:28:48 -0400

    add component to job_openings page - Description.tsx

---
+++ components/corporation/job_openings/Description.tsx
@@ -0,0 +1,39 @@
+const Description = () => {
+  return (
+    <section className="p-5 mt-10">
+      <div>
+        <p className="text-sm md:text-base lg:text-lg my-3 font-medium">
+          Flipp is a pre-IPO company on a mission to help shoppers provide for
+          their families by making life more affordable by leveraging
+          technology. We work with the largest retailers and manufacturers in
+          the world to help them transform their businesses and connect them
+          with millions of shoppers through our next-generation digital shopping
+          marketplace. Our award-winning Flipp app is used by millions of
+          shoppers every week to make better purchase decisions and to save
+          20-40% off their bills.
+        </p>
+        <p className="text-sm md:text-base lg:text-lg my-3 font-medium">
+          With the recent increase in costs of living, Flipp’s mission has never
+          been more relevant. Flipp’s Shopper Consideration Platform allows
+          retailers and manufacturers to create digital experiences from their
+          savings & deals content. These experiences help Shoppers decide what
+          to buy and where to buy it. Flipp then connects those retailer and
+          manufacturer experiences to millions of shoppers through Flipp’s owned
+          and operated channels.
+        </p>
+        <p className="text-sm md:text-base lg:text-lg my-3 font-medium">
+          Our culture is at the centre of everything we do. We’re honoured to
+          have been recognized through numerous accolades over the past three
+          years including{" "}
+          <span className="font-bold">
+            Best Workplaces in Canada, Deloitte 50 Best Managed, Best Workplaces
+            in Canada for Women, Most Admired CEO and Top 10 Most Admired
+            Corporate Cultures.
+          </span>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Description;
